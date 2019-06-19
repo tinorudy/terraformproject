645e4c240fdb7aa0e676b6b3d5e345ec704b1c4f
tinorudy <ayodejifagbule@gmail.com>
2019-06-19 15:04:17 +0100

    modules

---
+++ Modules/VPC/var.tf
@@ -0,0 +1,22 @@
+variable "vpc_cidr" {}
+
+
+   ##default = "10.0.0.0/16"
+
+
+variable "tenancy" {
+   default = "dedicated"
+}
+
+variable "vpc_id" {}
+
+variable "subnet_cidr" {}
+
+variable "subnet_cidr1" {}
+
+
+   ##default = "10.0.1.0/28"
+
+variable "name" {
+   default = "igw"
+}
